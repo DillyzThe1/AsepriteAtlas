9427497153dd8a77d69c03286f0c86d51803cba2
DillyzThe1 <dyl1029an@gmail.com>
2022-09-02 18:15:01 -0500

    test state (no frames)

---
+++ source/test/AtlasTestState.hx
@@ -0,0 +1,18 @@
+package test;
+
+import flixel.FlxSprite;
+import flixel.FlxState;
+
+class AtlasTestState extends FlxState
+{
+	public var testSprite:FlxSprite;
+
+	override public function create()
+	{
+		super.create();
+
+		testSprite = new FlxSprite();
+		testSprite.frames = null;
+		add(testSprite);
+	}
+}
--- source/test/Main.hx
+++ source/test/Main.hx
@@ -1,6 +1,7 @@
 package test;
 
 import flixel.FlxGame;
+import openfl.display.FPS;
 import openfl.display.Sprite;
 
 class Main extends Sprite
@@ -8,5 +9,7 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
+		addChild(new FlxGame(0, 0, AtlasTestState, 1, #if html5 60, 60 #else 120, 120 #end, true, false));
+		addChild(new FPS());
 	}
 }
